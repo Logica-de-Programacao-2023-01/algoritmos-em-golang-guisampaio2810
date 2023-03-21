6276d70e9b01d77f1140da71582163e79aeed307
Guilherme Sampaio de Andrade Carneiro <guisampaio@dinfor.uniceub>
2023-03-20 23:18:46 -0300

    sequencial9

---
+++ Sequencial9.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	var precoProduto float64
+	fmt.Print("Qual preço do produto?")
+	fmt.Scan(&precoProduto)
+	fmt.Println("O preço do produto com 10% é de:" precoProduto * 0.9)
+}
